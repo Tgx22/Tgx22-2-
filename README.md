0cece15af2df83333389d6cd8f212d20b9e8da38
Tgx22 <98214562+Tgx22@users.noreply.github.com>
2022-01-26 14:40:18 +0100

    Update and rename README.md to TooL.py

--- README.md
+++
@@ -1,4 +0,0 @@
-# Tgx22-2-
-Welcome to all freindes .. !! 
-I am Tgx22 by ^_^  
-I love all ...❤❤
---
+++ TooL.py
@@ -0,0 +1,20 @@
+# Tgx22-2-
+import os 
+import time
+os.system("clear")
+os.system("date")
+os.system("time")
+time.sleep(3.5)
+print("____________________________________________________________________________________________________")
+print("")
+times.sleep(2.5)
+print("Welcome To All Freindes .. ")
+print("")
+print("Can You Go To MY GitHub And Give My a Start.. ")
+time.sleep(2.5)
+print("")
+print("Thank you ..❤❤")
+time.sleep(3.5)
+print("")
+time.sleep(2.5)
+print("By ^_^")
